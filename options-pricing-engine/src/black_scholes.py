04ed2394e04e22d2c96721d25cf207d01f81f8ec
Sanika Kinare <sanikakinare@Sanikas-MacBook-Air.local>
2026-08-01 16:53:15 -0400

    Black Scholes - European Options

--- options-pricing-engine/src/black_scholes.py
+++ options-pricing-engine/src/black_scholes.py
@@ -55,4 +55,17 @@ def black_scholes_price(S0, K, r, sigma, T, option_type="call"):
     come out to about 10.4506 (see config.BS_CALL_REFERENCE).
     -------------------------------------------------------------------------
     """
-    raise NotImplementedError("Implement black_scholes_price in Monday's session.")
+    # d1 & d2 formulas
+    d1 = (np.log(S0/K) +(r+sigma**2 *0.5)*T)/(sigma * T**0.5)
+    d2 = d1 - sigma * T**0.5
+
+    option_price = 0
+
+    if option_type == "call":
+        option_price = S0 * norm.cdf(d1) - K * np.exp(-r*T) * norm.cdf(d2)
+    elif option_type == "put":
+        option_price = K * np.exp(-r*T) * norm.cdf(-d2) - S0 * norm.cdf(-d1)
+    else:
+        raise ValueError("option_type must be 'call' or 'put'")
+
+    return option_price
