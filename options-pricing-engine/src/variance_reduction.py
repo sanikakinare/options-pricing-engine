32c2a3aced13fb944664924ac1dcac1e03bfcad4
Sanika Kinare <sanikakinare@Sanikas-MacBook-Air.local>
2026-09-20 14:49:15 -0400

    fix geometric asian for discrete

--- options-pricing-engine/src/variance_reduction.py
+++ options-pricing-engine/src/variance_reduction.py
@@ -48,23 +48,32 @@ def price_asian_antithetic(S0, K, r, sigma, T, n_paths, n_steps, option_type="ca
 def price_asian_control_variate(S0, K, r, sigma, T, n_paths, n_steps, option_type="call", seed=None):
     """Week 3 -- control-variate version using the geometric-Asian closed form."""
     # Kemna Vorst
-    effective_sigma = sigma / np.sqrt(3)
-    effective_drift = 0.5 * (r - sigma**2/6)
+    # effective_sigma = sigma / np.sqrt(3)
+    # effective_drift = 0.5 * (r - sigma**2/6)
 
-    d1 = (np.log(S0/K) + (effective_drift + effective_sigma**2/2) * T)/(effective_sigma * np.sqrt(T))
-    d2 = d1 - effective_sigma * np.sqrt(T)
+    # d1 = (np.log(S0/K) + (effective_drift + effective_sigma**2/2) * T)/(effective_sigma * np.sqrt(T))
+    # d2 = d1 - effective_sigma * np.sqrt(T)
+
+    # Kemna Vorst with n steps - unbiased control variate
+    dt = T/n_steps
+    mean = np.log(S0) + (r - 0.5 * sigma**2) * dt * (n_steps + 1) / 2
+    variance = sigma**2 * dt * (n_steps + 1) * (2 * n_steps + 1) / (6 * n_steps)
+    d1 = (mean - np.log(K) + variance) / np.sqrt(variance)
+    d2 = d1 - np.sqrt(variance)
 
     if option_type == "call":
-        option_price = S0 * np.exp((effective_drift - r)*T)* norm.cdf(d1) - K * np.exp(-r*T) * norm.cdf(d2)
-    elif option_type == "put":
-        option_price = K * np.exp(-r*T) * norm.cdf(-d2) - S0 *  np.exp((effective_drift - r)*T)* norm.cdf(-d1)
-
+        option_price = np.exp(-r*T) * (np.exp(mean + variance/2) * norm.cdf(d1) - K * norm.cdf(d2))
+    else:
+        option_price = np.exp(-r*T) * (K * norm.cdf(-d2) - np.exp(mean + variance/2) * norm.cdf(-d1))
+    
     S_path = simulate_paths(S0, r, sigma, T, n_paths, n_steps, seed=seed)
     geometric_price = geometric_asian_mc(S_path, n_steps, K, r, T, option_type=option_type)
     arithmetic_price = arithmetic_asian_mc(S_path, K, r, T, option_type=option_type)
 
-    control_variate_price = arithmetic_price + (option_price - geometric_price)
-    return control_variate_price
+    coeff = np.cov(arithmetic_price, geometric_price)[0, 1]/np.var(geometric_price, ddof=1)
+
+    control_variate_price = arithmetic_price - coeff * (geometric_price - option_price)
+    return control_variate_price.mean()
 
 def simulate_paths(S0, r, sigma, T, n_paths, n_steps, seed=None):
     rng = np.random.default_rng(seed)
@@ -85,16 +94,16 @@ def arithmetic_asian_mc(S_paths, K, r, T, option_type="call"):
     else:
         payoff = np.maximum(K - S_average, 0)
 
-    discounted_payoff = np.mean(payoff) * np.exp(-r*T)
+    discounted_payoff = payoff * np.exp(-r*T)
     return discounted_payoff
 
 def geometric_asian_mc(S_paths, n_steps, K, r, T, option_type = "call"):
-    S_geometric = np.prod(S_paths, axis = 1) ** (1/n_steps)
+    S_geometric = np.exp(np.mean(np.log(S_paths), axis=1))
 
     if option_type == "call":
         payoff = np.maximum(S_geometric - K, 0)
     else:
         payoff = np.maximum(K - S_geometric, 0)
 
-    discounted_payoff = np.mean(payoff) * np.exp(-r*T)
+    discounted_payoff = payoff * np.exp(-r*T)
     return discounted_payoff
